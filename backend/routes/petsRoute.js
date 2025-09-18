7c0ae04b1521b471da773a80b202f90c2da09a79
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-09-17 22:45:50 -0400

    update pet

--- backend/routes/petsRoute.js
+++ backend/routes/petsRoute.js
@@ -6,29 +6,29 @@ const mongoose = require("mongoose");
 const Client = require("../models/Client");
 const Pet = require("../models/Pet");
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
     const petList = await Pet.find().populate("client");
 
-    if(!petList){
-        res.status(500).json({success: false});
+    if (!petList) {
+        res.status(500).json({ success: false });
     }
-        res.status(200).send(petList);
-}); 
+    res.status(200).send(petList);
+});
 
-router.get("/:id", async(req, res)=>{
+router.get("/:id", async (req, res) => {
     const pet = await Pet.findById(req.params.id).populate("client");
 
-    if(!pet){
-        res.status(404).json({message: "o animal de estimação com o ID fornecido não foi encontrado."})
+    if (!pet) {
+        res.status(404).json({ message: "o animal de estimação com o ID fornecido não foi encontrado." })
     }
 
     res.status(200.).send(pet);
 })
-    
 
-router.post("/", async (req, res)=> {
+
+router.post("/", async (req, res) => {
     const client = await Client.findById(req.body.client);
-    if(!client) return res.status(404).send("Cliente inválido");
+    if (!client) return res.status(404).send("Cliente inválido");
 
     let pet = new Pet({
         nome: req.body.nome,
@@ -46,9 +46,37 @@ router.post("/", async (req, res)=> {
     });
     pet = await pet.save();
 
-    if(!pet) return res.status(400).send("o pet não pode ser criado!");
+    if (!pet) return res.status(400).send("o pet não pode ser criado!");
 
     res.send(pet);
 });
+
+router.put("/:id", async (req, res) => {
+    const client = await Client.findById(req.body.client);
+    if (!client) return res.status(404).send("cliente não encontrado");
+
+    const pet = await Pet.findByIdAndUpdate(
+        req.params.id,
+        {
+            nome: req.body.nome,
+            especie: req.body.especie,
+            raca: req.body.raca,
+            cor: req.body.cor,
+            altura: req.body.altura,
+            peso: req.body.peso,
+            sexo: req.body.sexo,
+            dataNascimento: req.body.dataNascimento,
+            pai: req.body.pai,
+            mae: req.body.mae,
+            observacoes: req.body.observacoes,
+            client: client,
+        },
+        {new: true} //retorna objeto atualizado
+    );
+
+    if(!pet) return res.status(400).send("o pet não pode ser atualizado!");
+
+    res.send(pet);
+})
 
 module.exports = router;
