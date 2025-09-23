ac4edf712923c0d5e9c942a264e0d95fe6d7bb7f
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-09-22 23:05:05 -0400

    get veterinario

--- backend/routes/vetsRoutes.js
+++ backend/routes/vetsRoutes.js
@@ -3,6 +3,24 @@ const router = express.Router();
 
 const Vet = require("../models/Vet");
 
+router.get("/", async(req, res) => {
+    const vetList = await Vet.find();
+
+    if(!vetList) {
+        res.status(500).json({success: false});
+    }
+    res.status(200).send(vetList);
+});
+
+router.get("/:id", async(req, res)=> {
+    const vet = await Vet.findById(req.params.id);
+
+    if(!vet){
+        res.status(404).json({message: "o veterinário com o id informado não foi encontrado!"});
+    }
+    res.status(200).send(vet);
+});
+
 router.post("/", async (req, resp) => {
     let vet = new Vet({
         nome: req.body.nome,
