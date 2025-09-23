177ab18ea9a8cb814514bfbe7da02257eb2d81db
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-09-22 22:54:44 -0400

    post veterinário

--- backend/routes/vetsRoutes.js
+++ backend/routes/vetsRoutes.js
@@ -3,4 +3,18 @@ const router = express.Router();
 
 const Vet = require("../models/Vet");
 
+router.post("/", async (req, resp) => {
+    let vet = new Vet({
+        nome: req.body.nome,
+        email: req.body.email,
+        celular: req.body.celular,
+        endereco: req.body.endereco,
+        estado: req.body.estado,
+    });
+    vet = await vet.save();
+
+    if (!vet) return resp.status(400).send("o veterinário não pode ser criadoo!");
+    resp.send(vet);
+});
+
 module.exports = router;
